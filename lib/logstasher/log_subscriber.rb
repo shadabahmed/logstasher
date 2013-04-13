1dcb9f6182abbeea9143c53cda9ae7e74f893dc3
Shadab Ahmed <shadab_ahmed@mckinsey.com>
2013-04-13 21:58:27 +0530

    New line missing from json output Fix

--- lib/logstasher/log_subscriber.rb
+++ lib/logstasher/log_subscriber.rb
@@ -15,7 +15,7 @@ module LogStasher
 
       event = LogStash::Event.new('@fields' => data, '@tags' => ['request'])
       event.tags << 'exception' if payload[:exception]
-      LogStasher.logger << event.to_json
+      LogStasher.logger << event.to_json + "\n"
     end
 
     def redirect_to(event)
