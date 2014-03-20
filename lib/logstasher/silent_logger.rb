df1776a26521629f941944219bb5e6d6d0cbc65e
Devin Christensen <quixoten@gmail.com>
2014-03-20 14:25:47 -0600

    Re-factor silent logger
    Simply including a module does not override methods already defined on
    the class.

--- lib/logstasher/silent_logger.rb
+++ lib/logstasher/silent_logger.rb
@@ -1,12 +1,16 @@
 module LogStasher
   module SilentLogger
-    def logger
-      unless @logstasher_silent_logger
-        @logstasher_silent_logger       = ::Logger.new('/dev/null')
-        @logstasher_silent_logger.level = ::Logger::UNKNOWN
-      end
+    def self.included(klass)
+      klass.class_eval do
+        def logger
+          unless @logstasher_silent_logger
+            @logstasher_silent_logger       = ::Logger.new('/dev/null')
+            @logstasher_silent_logger.level = ::Logger::UNKNOWN
+          end
 
-      @logstasher_silent_logger
+          @logstasher_silent_logger
+        end
+      end
     end
   end
 end
