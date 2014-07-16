756fdb09678133f42c835a160b0cc99b5aa50cf9
Jan Szumiec <jan.szumiec@gmail.com>
2014-07-16 16:27:26 +0100

    Fixed failing test for ActionMailer/Rails with no templates.

--- spec/support/sample_mailer.rb
+++ spec/support/sample_mailer.rb
@@ -2,9 +2,15 @@ require 'action_mailer'
 
 class SampleMailer < ActionMailer::Base
   def welcome
-    mail(from: 'some-dude@example.com', to: 'some-other-dude@example.com', subject: 'Hello, there')
+    mail(from: 'some-dude@example.com', to: 'some-other-dude@example.com', subject: 'Hello, there') do |format|
+      format.text { render plain: 'OK' }
+    end
   end
 
   def receive(email)
+  end
+
+  def _render_template(_)
+    ""
   end
 end
