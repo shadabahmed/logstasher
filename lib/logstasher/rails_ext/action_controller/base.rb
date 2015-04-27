b27ae179a57b87c505e868b40d644592b7238f72
Marc Grimme <grimme@fidor.de>
2015-04-27 10:30:15 +0200

    Added support to include logstasher in controller with needing the monkey patch to ActionController#process_action

---
+++ lib/logstasher/rails_ext/action_controller/base.rb
@@ -0,0 +1,47 @@
+module LogStasher
+  module ActionController
+    module Instrumentation
+      def process_action(*args)
+        add_custom_fields_to_store
+        LogStasher.clear_request_context
+        LogStasher.add_default_fields_to_request_context(request)
+
+        super(*args)
+      end
+      
+      private
+
+      def append_info_to_payload(payload) #:nodoc:
+        LogStasher.add_default_fields_to_payload(payload, request)
+        if self.respond_to?(:logstasher_add_custom_fields_to_request_context)
+          logstasher_add_custom_fields_to_request_context(LogStasher.request_context)
+        end
+
+        if self.respond_to?(:logtasher_add_custom_fields_to_payload)
+          before_keys = payload.keys.clone
+          logtasher_add_custom_fields_to_payload(payload)
+          after_keys = payload.keys
+          # Store all extra keys added to payload hash in payload itself. This is a thread safe way
+          LogStasher.custom_fields += after_keys - before_keys
+        end
+
+        payload[:status] = response.status
+        super(payload)
+
+        LogStasher.store.each do |key, value|
+          payload[key] = value
+        end
+
+        LogStasher.request_context.each do |key, value|
+          payload[key] = value
+        end
+      end
+      
+      def add_custom_fields_to_store
+        LogStasher.store[:ip] = request.remote_ip
+        LogStasher.store[:route] = "#{request.params[:controller]}##{request.params[:action]}"
+        LogStasher.request_context[:request_id] = request.env['action_dispatch.request_id']
+      end
+    end
+  end
+end
---
+++ spec/lib/logstasher/base_instrumentation_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+require 'logstasher/rails_ext/action_controller/base'
+
+describe LogStasher::SampleController do
+  before do
+    module ActionController              # Revert the monkey patch again
+      module Instrumentation
+        alias :process_action :orig_process_action
+      end
+    end
+  end
+
+  before :each do
+
+    subject.request = ActionDispatch::TestRequest.new
+    subject.response = ActionDispatch::TestResponse.new
+
+    def subject.index(*args)
+      render text: 'OK'
+    end
+  end
+
+  describe ".process_action" do
+    it "adds default fields to payload" do
+      expect(LogStasher).to receive(:add_default_fields_to_payload).once
+      expect(LogStasher).to receive(:add_default_fields_to_request_context).once
+      subject.process_action(:index)
+    end
+
+    it "creates the request context before processing" do
+      LogStasher.request_context[:some_key] = 'value'
+      expect(LogStasher).to receive(:clear_request_context).once.and_call_original
+      expect {
+        subject.process_action(:index)
+      }.to change { LogStasher.request_context }
+    end
+
+    it "notifies rails of a request coming in" do
+      expect(ActiveSupport::Notifications).to receive(:instrument).with("start_processing.action_controller", anything).once
+      expect(ActiveSupport::Notifications).to receive(:instrument).with("process_action.action_controller", anything).once
+      subject.process_action(:index)
+    end
+
+    context "request context has custom fields defined" do
+      before :each do
+        LogStasher.add_custom_fields_to_request_context do |fields|
+          fields[:some_field] = 'value'
+        end
+
+        ActiveSupport::Notifications.subscribe('process_action.action_controller') do |_, _, _, _, payload|
+          @payload = payload
+        end
+      end
+
+      it "should retain the value in the request context" do
+        subject.process_action(:index)
+      end
+
+      after :each do
+        expect(@payload[:some_field]).to eq('value')
+
+        ActionController::Metal.class_eval do
+          undef logstasher_add_custom_fields_to_request_context
+        end
+        ActionController::Base.class_eval do
+          undef logstasher_add_custom_fields_to_request_context
+        end
+      end
+    end
+  end
+end
