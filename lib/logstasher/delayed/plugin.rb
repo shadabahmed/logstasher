59ace36c15777dab3fd87b2f60a97d6c188cb1a3
Marc Grimme <grimme@fidor.de>
2015-05-13 09:55:22 +0200

    Remove source from delayed job plugin. Should be done outside.

--- lib/logstasher/delayed/plugin.rb
+++ lib/logstasher/delayed/plugin.rb
@@ -5,7 +5,6 @@ module LogStasher
       callbacks do |lifecycle|
         lifecycle.before(:invoke_job) do |job, *args, &block|
           ::LogStasher.request_context[:request_id] = job.id
-          ::LogStasher.source = "Delayed::Job"
           # perhaps handler#object.first_line and method_name would be interesting too (in store??)
         end
         lifecycle.after(:invoke_job) do |job, *args, &block|
