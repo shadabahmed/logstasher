b4121e826710e2c2e3a991bc25498137852b1ca4
Jim Durand <me@jdurand.com>
2016-01-27 23:14:58 -0500

    Make sure rack_cache[:verbose] can be set
    This exploded for me.
    Copied a commit from lograge which solved the same issue:
    https://github.com/roidrage/lograge/pull/103

--- lib/logstasher/railtie.rb
+++ lib/logstasher/railtie.rb
@@ -8,7 +8,7 @@ module LogStasher
     config.logstasher.enabled = false
 
     initializer :logstasher, :before => :load_config_initializers do |app|
-      app.config.action_dispatch.rack_cache[:verbose] = false if app.config.action_dispatch.rack_cache
+      app.config.action_dispatch.rack_cache[:verbose] = false if rack_cache_hashlike?(app)
       LogStasher.setup_before(app.config.logstasher) if app.config.logstasher.enabled
     end
 
@@ -16,6 +16,10 @@ module LogStasher
       config.after_initialize do
         LogStasher.setup(config.logstasher) if config.logstasher.enabled
       end
+    end
+
+    def rack_cache_hashlike?(app)
+      app.config.action_dispatch.rack_cache && app.config.action_dispatch.rack_cache.respond_to?(:[]=)
     end
   end
 end
