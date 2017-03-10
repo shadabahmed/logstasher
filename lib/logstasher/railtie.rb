dcf1b9272877bcd6768d2e97a3d3d3d11984bb2e
Yuji Hanamura <yuji.developer@gmail.com>
2017-03-10 14:13:55 +0900

    Fix default value of source option

--- lib/logstasher/railtie.rb
+++ lib/logstasher/railtie.rb
@@ -4,43 +4,6 @@ require 'action_controller/log_subscriber'
 require 'socket'
 
 module LogStasher
-  class Railtie < Rails::Railtie
-    config.logstasher = ::ActiveSupport::OrderedOptions.new
-    config.logstasher.enabled = false
-
-    # Set up the default logging options
-    config.logstasher.controller_enabled = true
-    config.logstasher.mailer_enabled = true
-    config.logstasher.record_enabled = false
-    config.logstasher.view_enabled = true
-    config.logstasher.job_enabled = true
-
-    # Try loading the config/logstasher.yml if present
-    env = Rails.env.to_sym || :development
-    config_file = File.expand_path "./config/logstasher.yml"
-
-    # Load and ERB templating of YAML files
-    LOGSTASHER = File.exists?(config_file) ? YAML.load(ERB.new(File.read(config_file)).result).symbolize_keys : nil
-
-    initializer :logstasher, :before => :load_config_initializers do |app|
-      if LOGSTASHER.present?
-        # process common configs
-        LogStasher.process_config(app.config.logstasher, LOGSTASHER)
-        # process environment specific configs
-        LogStasher.process_config(app.config.logstasher, LOGSTASHER[env].symbolize_keys) if LOGSTASHER.key? env
-      end
-
-      app.config.action_dispatch.rack_cache[:verbose] = false if app.config.action_dispatch.rack_cache
-      LogStasher.setup_before(app.config.logstasher) if app.config.logstasher.enabled
-    end
-
-    initializer :logstasher do
-      config.after_initialize do
-        LogStasher.setup(config.logstasher) if config.logstasher.enabled
-      end
-    end
-  end
-
   def default_source
     case RUBY_PLATFORM
     when /darwin/
@@ -69,10 +32,48 @@ module LogStasher
     config.suppress_app_log = yml_config[:suppress_app_log] if yml_config.key? :suppress_app_log
 
     # This line is optional, it allows you to set a custom value for the @source field of the log event
-    config.source = yml_config.key?(:source) ? yml_config[:source] : default_source
+    config.source = yml_config[:source] if yml_config.key? :source
 
     config.backtrace = yml_config[:backtrace] if yml_config.key? :backtrace
     config.logger_path = yml_config[:logger_path] if yml_config.key? :logger_path
     config.log_level = yml_config[:log_level] if yml_config.key? :log_level
+  end
+
+  class Railtie < Rails::Railtie
+    config.logstasher = ::ActiveSupport::OrderedOptions.new
+    config.logstasher.enabled = false
+
+    # Set up the default logging options
+    config.logstasher.controller_enabled = true
+    config.logstasher.mailer_enabled = true
+    config.logstasher.record_enabled = false
+    config.logstasher.view_enabled = true
+    config.logstasher.job_enabled = true
+    config.logstasher.source = LogStasher.default_source
+
+    # Try loading the config/logstasher.yml if present
+    env = Rails.env.to_sym || :development
+    config_file = File.expand_path "./config/logstasher.yml"
+
+    # Load and ERB templating of YAML files
+    LOGSTASHER = File.exists?(config_file) ? YAML.load(ERB.new(File.read(config_file)).result).symbolize_keys : nil
+
+    initializer :logstasher, :before => :load_config_initializers do |app|
+      if LOGSTASHER.present?
+        # process common configs
+        LogStasher.process_config(app.config.logstasher, LOGSTASHER)
+        # process environment specific configs
+        LogStasher.process_config(app.config.logstasher, LOGSTASHER[env].symbolize_keys) if LOGSTASHER.key? env
+      end
+
+      app.config.action_dispatch.rack_cache[:verbose] = false if app.config.action_dispatch.rack_cache
+      LogStasher.setup_before(app.config.logstasher) if app.config.logstasher.enabled
+    end
+
+    initializer :logstasher do
+      config.after_initialize do
+        LogStasher.setup(config.logstasher) if config.logstasher.enabled
+      end
+    end
   end
 end
