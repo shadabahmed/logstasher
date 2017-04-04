02284a949f67de12a7a68f491e85d06a1929b8d8
Erik Anderson <edawebdesign@gmail.com>
2017-04-04 12:07:39 -0600

    Update log_subscriber.rb

--- lib/logstasher/log_subscriber.rb
+++ lib/logstasher/log_subscriber.rb
@@ -57,7 +57,7 @@ module LogStasher
         :controller => payload[:controller],
         :format     => extract_format(payload),
         :ip         => request.remote_ip,
-        :request_id => request.action_dispatch.request_id,
+        :request_id => request["action_dispatch\.request_id"],
         :method     => payload[:method],
         :path       => extract_path(payload),
         :route      => "#{payload[:controller]}##{payload[:action]}"
