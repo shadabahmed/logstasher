09bacfcd88c9199f2fd680ac7b32408cb3e21d5c
Peter Goldstein <peter@valimail.com>
2020-12-26 13:56:29 -0800

    Increase duration range

--- spec/lib/logstasher/active_job/log_subscriber_spec.rb
+++ spec/lib/logstasher/active_job/log_subscriber_spec.rb
@@ -24,7 +24,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
     logger
   end
 
-  before(:each) do
+  before do
     LogStasher.logger = logger
     LogStasher.request_context[:request_id] = 'foobar123'
 
@@ -59,8 +59,8 @@ describe LogStasher::ActiveJob::LogSubscriber do
 
   it 'logs 3 lines per job' do
     # enqueue, perform_start, perform
+    expect(log_output.json.size).to eq(0)
     enqueue_job
-    pp log_output.json
     expect(log_output.json.size).to eq(3)
   end
 
@@ -90,7 +90,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
       expect(json['queue_name']).to eq('Inline(default)')
       expect(json['job_class']).to eq('ActiveJobTestClass')
       expect(json['job_args']).to eq([{'error' => true}])
-      expect(json['duration']).to be_between(0, 1)
+      expect(json['duration']).to be_between(0, 2)
       expect(json['exception']).to eq(['ZeroDivisionError', 'divided by 0'])
       expect(json).to_not have_key('scheduled_at')
     end
