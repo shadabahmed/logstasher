019c4ffba64ce02e8c52c43fa9d642191c431c62
Peter Goldstein <peter@valimail.com>
2020-12-25 23:24:03 -0800

    Switch to inline adapter so jobs are actually executed.

--- spec/lib/logstasher/active_job/log_subscriber_spec.rb
+++ spec/lib/logstasher/active_job/log_subscriber_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'logstasher/active_job/log_subscriber'
+require 'active_job'
 
 
 describe LogStasher::ActiveJob::LogSubscriber do 
@@ -73,6 +74,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
   it 'logs 3 lines per job' do
     # enqueue, perform_start, perform
     enqueue_job
+    pp log_output.json
     expect(log_output.json.size).to eq(3)
   end
 
@@ -81,11 +83,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
     log_line('perform').tap do |json|
       expect(json['tags']).to eq(['job', 'perform'])
       expect(json['job_id']).to eq(job.job_id)
-<<<<<<< HEAD
       expect(json['queue_name']).to eq('Test(default)')
-=======
-      expect(json['queue_name']).to eq('Async(default)')
->>>>>>> 83c55a1... Some initial fixes to get specs running under modern Rails versions
       expect(json['job_class']).to eq('ActiveJobTestClass')
       expect(json['job_args']).to eq([1,2,3, {'a' => {'b' => ['c']}}])
       expect(json['duration']).to be_between(0, 1)
@@ -104,11 +102,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
       expect(json['tags']).to eq(['job', 'perform', 'exception'])
       expect(json['job_id']).to be_a(String) # Don't have good access to the real id in this test
       expect(json['job_id']).to_not eq('foobar123')
-<<<<<<< HEAD
-      expect(json['queue_name']).to eq('Test(default)')
-=======
-      expect(json['queue_name']).to eq('Async(default)')
->>>>>>> 83c55a1... Some initial fixes to get specs running under modern Rails versions
+      expect(json['queue_name']).to eq('Inline(default)')
       expect(json['job_class']).to eq('ActiveJobTestClass')
       expect(json['job_args']).to eq([{'error' => true}])
       expect(json['duration']).to be_between(0, 1)
@@ -122,11 +116,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
     log_line('perform_start').tap do |json|
       expect(json['tags']).to eq(['job', 'perform_start'])
       expect(json['job_id']).to eq(job.job_id)
-<<<<<<< HEAD
-      expect(json['queue_name']).to eq('Test(default)')
-=======
-      expect(json['queue_name']).to eq('Async(default)')
->>>>>>> 83c55a1... Some initial fixes to get specs running under modern Rails versions
+      expect(json['queue_name']).to eq('Inline(default)')
       expect(json['job_class']).to eq('ActiveJobTestClass')
       expect(json['job_args']).to eq([1,2,3, {'a' => {'b' => ['c']}}])
       expect(json).to_not have_key('duration')
@@ -140,11 +130,7 @@ describe LogStasher::ActiveJob::LogSubscriber do
     log_line('enqueue').tap do |json|
       expect(json['tags']).to eq(['job', 'enqueue'])
       expect(json['job_id']).to eq(job.job_id)
-<<<<<<< HEAD
-      expect(json['queue_name']).to eq('Test(default)')
-=======
-      expect(json['queue_name']).to eq('Async(default)')
->>>>>>> 83c55a1... Some initial fixes to get specs running under modern Rails versions
+      expect(json['queue_name']).to eq('Inline(default)')
       expect(json['job_class']).to eq('ActiveJobTestClass')
       expect(json['job_args']).to eq([1,2,3, {'a' => {'b' => ['c']}}])
       expect(json).to_not have_key('duration')
