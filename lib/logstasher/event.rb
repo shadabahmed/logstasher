689346addf37a7bd7d5a6befc5dcdb6e021f0618
Shadab Ahmed <shadab.ansari@gmail.com>
2020-12-27 18:47:58 -0800

    Revert back event.rb to master

--- lib/logstasher/event.rb
+++ lib/logstasher/event.rb
@@ -1,24 +1,21 @@
-require "json"
-require "time"
-require "date"
+require 'json'
+require 'time'
+require 'date'
 
 module LogStasher
   class Event
-
-    def initialize(data={})
+    def initialize(data = {})
       @cancelled = false
 
       @data = data
-      if data.include?("@timestamp")
-        t = data["@timestamp"]
-        if t.is_a?(String)
-          data["@timestamp"] = Time.parse(t).gmtime.iso8601(3)
-        end
+      if data.include?('@timestamp')
+        t = data['@timestamp']
+        data['@timestamp'] = Time.parse(t).gmtime.iso8601(3) if t.is_a?(String)
       else
-        data["@timestamp"] = ::Time.now.utc.iso8601(3)
+        data['@timestamp'] = ::Time.now.utc.iso8601(3)
       end
-      data["@version"] = "1" if !@data.include?("@version")
-    end 
+      data['@version'] = '1' unless @data.include?('@version')
+    end
 
     def to_s
       to_json.to_s
