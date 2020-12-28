aac51d2c036dac3ca766f48744fa0747a6bb533e
Shadab Ahmed <shadab.ansari@gmail.com>
2020-12-28 10:34:18 -0800

    Trim spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,18 +11,10 @@ end
 # Set rails env as test
 ENV['RAILS_ENV'] = 'test'
 
-# Modify load path so you can require 'logstasher directly.
-$LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
-
-require 'rubygems'
-# Loads bundler setup tasks. Now if I run spec without installing gems then it would say gem not installed and
-# do bundle install instead of ugly load error on require.
-
 # This will require me all the gems automatically for the groups. If I do only .setup then I will have to require gems
 # manually. Note that you have still have to require some gems if they are part of bigger gem like ActiveRecord which is
 # part of Rails. You can say :require => false in gemfile to always use explicit requiring
 Bundler.require(:default, :test)
-require 'logstasher'
 require 'active_support/testing/assertions'
 Dir[File.join('./spec/support/**/*.rb')].each { |f| require f }
 
@@ -35,8 +27,4 @@ RSpec.configure do |config|
   config.filter_run :focus
 
   ActiveJob::Base.queue_adapter = :test
-
-  def assert_nothing_raised(&block)
-    expect(&block).to_not raise_error
-  end
 end
