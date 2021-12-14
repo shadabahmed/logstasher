0500bc2321d88c1d7638ec0bd3c3e6145c066bfb
Jon Philpott <jon.philpott@mx.com>
2021-12-14 14:38:39 -0700

    add simple udp logging device.

---
+++ lib/logstasher/device/udp.rb
@@ -0,0 +1,40 @@
+# simple UDP logger
+
+require 'logstasher/device'
+require 'socket'
+
+
+module LogStasher
+  module Device
+    class UDP
+      include ::LogStasher::Device
+
+      attr_reader :options, :socket
+
+      def initialize(options = {})
+        @options = default_options.merge(stringify_keys(options))
+        @socket = UDPSocket.new
+      end
+
+      def close
+        @socket.close
+      end
+
+      def write(log)
+        @socket.send(log, 0, options['hostname'], options['port'])
+      end
+
+      private
+
+      def default_options
+        {
+          'hostname' => '127.0.0.1',
+          'port'     => 31459,
+        }
+      end
+    end
+  end
+end
+
+
+
